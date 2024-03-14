d9c642ac6a326647edfc639ab62bed45c07ed036
Abhishek <Abhishekraut8697@gmail.com>
2024-03-14 23:44:59 +0530

    Renamed script.js to string.js

--- Week 1/Day_2_Basic_JsApis/string.mjs
+++ Week 1/Day_2_Basic_JsApis/string.mjs
@@ -19,8 +19,42 @@ function findlastIndexOf(str,target){
 findlastIndexOf("Abhishek Raut Raut Raut",'Raut'); 
 findlastIndexOf("Abhishek Raut Raut Raut",'raut'); //getting -1 because it is case sensitive
 
+//_________________________________________________________
+
 const getSlice =(str,start,end)=>{
     console.log("Your Entered String is ",str);
     console.log(str.slice(start,end));
 }
 getSlice("hello world",0,6);
+
+function cutIt(str,startIndex,endIndex){
+    let newStr = '';
+    for(let i=0;i<str.length;i++){
+        if(i>=startIndex && i<endIndex){
+            newStr = newStr + str[i]
+        }
+    }
+    return newStr;
+}
+
+let ans = cutIt('Abhishek',0,4);
+console.log(ans);
+
+//_________________________________________________________
+
+let name = 'Abhishek Raut';
+console.log(name.replace("Abhishek","Chinu"))
+
+//Split
+let para = 'Hi my name is Abhi';
+console.log(para.split(" "))
+
+const val = '      abhi  dev  ';
+console.log(val.trim())
+
+
+//toUppper
+
+let dev = "abhiShek";
+console.log(dev.toLowerCase())
+console.log(dev.toUpperCase())
